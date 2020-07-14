34809e5ad4a6770b5a638dff4199db4a848b5630
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2020-07-14 17:22:50 -0300

    exercise - checking the sum of two numbers using v-if and v-else

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let app = new Vue({
     el: '#app',
     data: {
-       firstNumber: 10,
-       secondNumber: 15
+       firstNumber: Math.floor(Math.random() * 30),
+       secondNumber: Math.floor(Math.random() * 30),
+       result: ''
     }
 })
