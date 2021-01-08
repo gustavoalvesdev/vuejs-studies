b6fb52d835ea4d3e08d84affd144f92c6c1dd1cb
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2021-01-08 07:49:15 -0300

    directives shortcuts

--- script.js
+++ script.js
@@ -1,33 +1,11 @@
-function flag(country) {
-	return `<img src="countries/${country}.png" />`;
-}
-
-Vue.component('country', {
-    props: [
-        'flag',
-        'continent'
-    ],
-    template: `<p v-html='flag + continent'></p>`
-})
-
 let app = new Vue({
-    el: '#app',
-    data: {
-    	argentina: flag('argentina'),
-    	brazil: flag('brazil'),
-    	china: flag('china'),
-    	uk: flag('uk'),
-    	usa: flag('usa')
-    },
-    methods: {
-    	countries: function() {
-    		return [
-    			{flag: this.argentina, continent: 'South America'},
-    			{flag: this.brazil, continent: 'South America'},
-    			{flag: this.china, continent: 'Asia'},
-    			{flag: this.uk, continent: 'Europe'},
-    			{flag: this.usa, continent: 'North America'}
-    		]
-    	}
-    }
-})
+	el: '#app',
+	data: {
+		url: 'https://youtube.com'
+	},
+	methods: {
+		fazAlgumaCoisa: () => {
+			alert('Fez');
+		}
+	}
+});
